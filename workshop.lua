ac17aa0336ebf963342f80618876073b0d1dbfe7
McAttenzione <39492926+McAttenzione@users.noreply.github.com>
2018-05-21 17:32:29 +0200

    Add files via upload

---
+++ workshop.lua
@@ -0,0 +1,130 @@
+resource.AddWorkshop("1368035687") -- [TTTC] Classes for Trouble in Terrorist Town
+resource.AddWorkshop("1369546685") -- [TTTC]Class Pack #1
+resource.AddWorkshop("359372950") -- [TTT] Explosive Corpse
+resource.AddWorkshop("141103402") -- ttt_bb_teenroom_b2
+resource.AddWorkshop("131586546") -- ttt_bb_suburbia_b3
+resource.AddWorkshop("1388044920") -- [tttc]class_pack
+resource.AddWorkshop("1375209708") -- [TTT] The Fart Rocket Fixed Sound
+resource.AddWorkshop("1382077974") -- ttt_golden_deagle_hunt
+resource.AddWorkshop("1374144781") -- TTT Cloaking Device Droppable
+resource.AddWorkshop("1390489281") -- [tttc]thief_class
+resource.AddWorkshop("1383535678") -- ttt_small_m9k_pack
+resource.AddWorkshop("1371754616") -- formyserver
+resource.AddWorkshop("196978840") -- ttt_old_factory
+resource.AddWorkshop("144575800") -- ttt_community_pool_revamped
+resource.AddWorkshop("131667838") -- ttt_community_bowling
+resource.AddWorkshop("1092797648") -- [TTT] Vampire Deagle
+resource.AddWorkshop("284613981") -- ttt_castle_2011_v3_day
+resource.AddWorkshop("922300051") -- TTT Poison Dart Gun
+resource.AddWorkshop("921953443") -- TTT Defibrillator
+resource.AddWorkshop("922348673") -- TTT Throwable Manhacks
+resource.AddWorkshop("888948962") -- [TTT] Freeze Gun
+resource.AddWorkshop("1370115226") -- [TTTC] Classes Pack inspired by TTT Fate
+resource.AddWorkshop("505893682") -- TTT Explosive Baby Launcher
+resource.AddWorkshop("376441841") -- TTT - Medkit (Traitor)
+resource.AddWorkshop("169342118") -- ttt_casino_b2
+resource.AddWorkshop("270698403") -- Night Trap - TTT, Murder
+resource.AddWorkshop("195227686") -- TTT_Dolls
+resource.AddWorkshop("258531409") -- ttt_eek_v6
+resource.AddWorkshop("157420728") -- ttt_waterworld
+resource.AddWorkshop("291180158") -- TTT - The Vampire
+resource.AddWorkshop("308966836") -- TTT - Death Faker
+resource.AddWorkshop("261934696") -- ttt_sunday_street_b2
+resource.AddWorkshop("482214902") -- ttt_trainstation_a5
+resource.AddWorkshop("494060881") -- Angry Hobo Swep for TTT
+resource.AddWorkshop("320963369") -- ttt_college
+resource.AddWorkshop("154177743") -- ttt_aircraft_v1b
+resource.AddWorkshop("1137482209") -- TTT Silentstep
+resource.AddWorkshop("933056549") -- TTT Sprint
+resource.AddWorkshop("574757288") -- GMN TTT Snowball
+resource.AddWorkshop("319562947") -- [TTT] BBY Launcher
+resource.AddWorkshop("295897079") -- ttt_lego
+resource.AddWorkshop("807428039") -- Holy Hand Greande + TTT
+resource.AddWorkshop("128091208") -- M9K Heavy Weapons
+resource.AddWorkshop("128093075") -- M9K Small Arms pack
+resource.AddWorkshop("128089118") -- M9K Assault Rifles
+resource.AddWorkshop("910490478") -- TTT - Thriller (Dart) 1 Shot
+resource.AddWorkshop("967237467") -- TTT Impact Pistol
+resource.AddWorkshop("398579898") -- TTT_Subway_AG
+resource.AddWorkshop("121935805") -- TTT_Vessel
+resource.AddWorkshop("899596699") -- TTT Flashbang (also for Detective)
+resource.AddWorkshop("140410125") -- TTT_67thway_v7
+resource.AddWorkshop("285372790") -- ttt_innocentmotel
+resource.AddWorkshop("1188011508") -- TTT - Amaterasu
+resource.AddWorkshop("1383661344") -- [TTTC] Class Pack By Hollyngton
+resource.AddWorkshop("284419411") -- TTT - Minifier
+resource.AddWorkshop("284418937") -- TTT - Jetpack
+resource.AddWorkshop("265678425") -- TTT_BrainParasite
+resource.AddWorkshop("620936792") -- TTT Homing Pigeon
+resource.AddWorkshop("606792331") -- TTT Advanced Disguiser
+resource.AddWorkshop("611911370") -- TTT Slowmotion with Icon
+resource.AddWorkshop("810154456") -- TTT Dead Ringer
+resource.AddWorkshop("934575429") -- TTT Addons for TTT Vote
+resource.AddWorkshop("788891000") -- TTT McCrees Peacekeeper (Its High Noon)
+resource.AddWorkshop("648957314") -- TTT Homerun Bat
+resource.AddWorkshop("662342819") -- TTT Randomat
+resource.AddWorkshop("653258161") -- TTT Blue Bull
+resource.AddWorkshop("621565420") -- TTT Door Buster
+resource.AddWorkshop("615091790") -- TTT Lemonade + LemonadeLauncher
+resource.AddWorkshop("680737032") -- TTT Prop Exploder
+resource.AddWorkshop("842302491") -- TTT Zombie Perk Bottles
+resource.AddWorkshop("672173225") -- TTT A Second Chance
+resource.AddWorkshop("676695745") -- TTT The Little Helper
+resource.AddWorkshop("611873052") -- TTT Mirror Fate
+resource.AddWorkshop("671603913") -- TTT Space and Time-Manipulator
+resource.AddWorkshop("151583504") -- MapVote - Fretta-like Map Voting
+resource.AddWorkshop("434164467") -- ttt_rooftops_a2
+resource.AddWorkshop("365340995") -- TTT DECAPITATION V2.1 No Sound!
+resource.AddWorkshop("942512444") -- TTT Drachenlord - Traitor Weapon
+resource.AddWorkshop("948384112") -- TTT Fart Grenade (Traitor)
+resource.AddWorkshop("266354490") -- ttt_parking_alley
+resource.AddWorkshop("273566208") -- TTT Minigun
+resource.AddWorkshop("104506140") -- ttt_stadium
+resource.AddWorkshop("147635981") -- ttt_forest_final
+resource.AddWorkshop("137891506") -- ttt_plaza_b7
+resource.AddWorkshop("282584080") -- TTT SpartanKick
+resource.AddWorkshop("738815378") -- [TTT] Genji's Katana
+resource.AddWorkshop("903823980") -- PietSmiet Bomb [TTT] by Shadow
+resource.AddWorkshop("855385475") -- PietSmiet Gun [TTT] by Shadow
+resource.AddWorkshop("596033434") -- The Present of Fate
+resource.AddWorkshop("1326662749") -- Ghost Knife (made for TTT)
+resource.AddWorkshop("937535488") -- TTT Prop Disguiser
+resource.AddWorkshop("255086323") -- ttt_elevatorandstairs
+resource.AddWorkshop("264985131") -- TTT Suburb / ttt_suburb_beta15a [Reupload]
+resource.AddWorkshop("1310924738") -- [TTT/SB] Supersheep
+resource.AddWorkshop("635911320") -- [TTT] Star Wars - The Force
+resource.AddWorkshop("639521512") -- [ttt] Boomerang
+resource.AddWorkshop("106404821") -- TTT Traitor Turtlenade
+resource.AddWorkshop("811718553") -- Thomas The Tank Engine TTT Weapon SWEP
+resource.AddWorkshop("500968461") -- TTT Goldenplix Prison (Prison Architect)
+resource.AddWorkshop("145316417") -- ttt_fastfood_a6
+resource.AddWorkshop("118937144") -- ttt_lttp_kakariko
+resource.AddWorkshop("802015788") -- Blink for TTT
+resource.AddWorkshop("801433502") -- Defibrillator for TTT
+resource.AddWorkshop("755748551") -- TTT Jarate
+resource.AddWorkshop("544977614") -- ttt_space_street_67_b2
+resource.AddWorkshop("253292930") -- ttt_cluedo_b5_improved1
+resource.AddWorkshop("643230441") -- [TTT/Sandbox] Mine Turtle (Proximity Mine)
+resource.AddWorkshop("253304826") -- ttt_camel_v1
+resource.AddWorkshop("254177306") -- [TTT] M4 SLAM (Tripmine + Remote C4)
+resource.AddWorkshop("646754302") -- [TTT/Sandbox] Gauss Rifle
+resource.AddWorkshop("253297309") -- ttt_airbus_b3
+resource.AddWorkshop("254177214") -- [TTT] Jihad Bomb (Charred Corpse)
+resource.AddWorkshop("281454209") -- ttt_Clue_se
+resource.AddWorkshop("1361102340") -- TTT - (Traitor) Harpoon
+resource.AddWorkshop("464069433") -- ttt_roathwholefoods
+resource.AddWorkshop("228998866") -- ttt_fuuk_jail_final2
+resource.AddWorkshop("229003350") -- ttt_residentevil2_se
+resource.AddWorkshop("557962280") -- ULX
+resource.AddWorkshop("557962238") -- ULib
+resource.AddWorkshop("300375321") -- Gun Stats for TTT and DarkRP
+resource.AddWorkshop("296631816") -- TTT_whitehouse_v9
+resource.AddWorkshop("183797802") -- TTT_island_2013
+resource.AddWorkshop("609729626") -- TTT Random Damage
+resource.AddWorkshop("332913972") -- ttt_fiskara_v3_beta
+resource.AddWorkshop("236217898") -- TTT DeathFaker
+resource.AddWorkshop("407751746") -- BearTrap for TTT
+resource.AddWorkshop("248949639") -- ttt_mall
+resource.AddWorkshop("691414646") -- TTT Cannibalism 2016
+resource.AddWorkshop("238369471") -- ttt_barons_v1
+resource.AddWorkshop("1101695410") -- TTT Burn Dem Dubstep Gun
